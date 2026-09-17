ed02aaba7870833e75effa18b0e86d95bf4175ca
prpttp54321 <perapat.t@behnmeyer.co.th>
2026-09-17 16:14:54 +0700

    Make other stakeholder option mutually exclusive and conditionally editable

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ document
         alert("ส่งแบบสอบถามเรียบร้อย");
 
     });
+
+// Make the "อื่น ๆ โปรดระบุ" field a single-select stakeholder option.
+const otherStakeholder = document.getElementById("otherStakeholder");
+const otherStakeholderGroup = otherStakeholder?.closest(".form-group");
+
+if (otherStakeholder && otherStakeholderGroup) {
+    const otherOption = document.createElement("label");
+    const otherRadio = document.createElement("input");
+
+    otherOption.className = "option-item other-option";
+    otherRadio.type = "radio";
+    otherRadio.name = "stakeholderType";
+    otherRadio.value = "Other";
+    otherRadio.id = "otherStakeholderOption";
+    otherRadio.setAttribute("aria-controls", "otherStakeholder");
+
+    otherOption.append(otherRadio, document.createTextNode("อื่น ๆ โปรดระบุ"));
+    otherStakeholderGroup.parentNode.insertBefore(otherOption, otherStakeholderGroup);
+
+    otherStakeholder.disabled = true;
+    otherStakeholder.setAttribute("aria-disabled", "true");
+
+    const stakeholderRadios = document.querySelectorAll('input[name="stakeholderType"]');
+
+    stakeholderRadios.forEach((radio) => {
+        radio.addEventListener("change", () => {
+            const isOtherSelected = otherRadio.checked;
+
+            otherStakeholder.disabled = !isOtherSelected;
+            otherStakeholder.required = isOtherSelected;
+            otherStakeholder.setAttribute("aria-disabled", String(!isOtherSelected));
+
+            if (!isOtherSelected) {
+                otherStakeholder.value = "";
+            }
+        });
+    });
+}
