66d0bd3e683fe466fa857dbf846e5bfcbcf715ea
prpttp54321 <perapat.t@behnmeyer.co.th>
2026-09-17 16:31:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,43 +8,40 @@ document
 
     });
 
-// Make "อื่น ๆ โปรดระบุ" look like a single radio option with an inline text field.
+const surveyForm = document.getElementById("surveyForm");
+
+const stakeholderRadios = document.querySelectorAll(
+    'input[name="stakeholderType"]'
+);
+
+const otherRadio = document.getElementById("otherStakeholderOption");
 const otherStakeholder = document.getElementById("otherStakeholder");
-const otherStakeholderGroup = otherStakeholder?.closest(".form-group");
 
-if (otherStakeholder && otherStakeholderGroup) {
-    const otherLabel = document.createElement("label");
-    const otherRadio = document.createElement("input");
+function updateOtherStakeholderField() {
+    const isOtherSelected = otherRadio.checked;
 
-    otherLabel.className = "option-item other-option";
-    otherRadio.type = "radio";
-    otherRadio.name = "stakeholderType";
-    otherRadio.value = "Other";
-    otherRadio.id = "otherStakeholderOption";
-    otherRadio.setAttribute("aria-controls", "otherStakeholder");
+    otherStakeholder.disabled = !isOtherSelected;
+    otherStakeholder.required = isOtherSelected;
+    otherStakeholder.setAttribute(
+        "aria-disabled",
+        String(!isOtherSelected)
+    );
 
-    otherStakeholder.placeholder = "Other";
-    otherStakeholder.disabled = true;
-    otherStakeholder.setAttribute("aria-disabled", "true");
-
-    otherLabel.append(otherRadio, otherStakeholder);
-    otherStakeholderGroup.replaceWith(otherLabel);
-
-    const stakeholderRadios = document.querySelectorAll('input[name="stakeholderType"]');
-
-    stakeholderRadios.forEach((radio) => {
-        radio.addEventListener("change", () => {
-            const isOtherSelected = otherRadio.checked;
-
-            otherStakeholder.disabled = !isOtherSelected;
-            otherStakeholder.required = isOtherSelected;
-            otherStakeholder.setAttribute("aria-disabled", String(!isOtherSelected));
-
-            if (isOtherSelected) {
-                otherStakeholder.focus();
-            } else {
-                otherStakeholder.value = "";
-            }
-        });
-    });
+    if (isOtherSelected) {
+        otherStakeholder.focus();
+    } else {
+        otherStakeholder.value = "";
+    }
 }
+
+stakeholderRadios.forEach((radio) => {
+    radio.addEventListener("change", updateOtherStakeholderField);
+});
+
+surveyForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    updateOtherStakeholderField();
+
+    alert("ส่งแบบสอบถามเรียบร้อย");
+});
